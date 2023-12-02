d7bd1ec305dbb8877913d75068ab3e3659891c0d
blienmoges <blienmoges92@gmail.com>
2023-12-03 01:32:35 +0300

    second version

---
+++ oddnumbers.js
@@ -0,0 +1,21 @@
+
+function printOddNumberPattern(n) {
+  if (n < 0) {
+    console.log("Please enter a non-negative number.");
+    return;
+  }
+
+  for (let i = 0; i <= n; i++) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+
+const numberToPrintUpTo = 10;
+printOddNumberPattern(numberToPrintUpTo);
+
+
+
+
