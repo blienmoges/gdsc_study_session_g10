009ce22eaf62337992a68d1e45c3a859ec660b02
blienmoges <blienmoges92@gmail.com>
2024-02-29 17:45:17 +0300

    edited bmi

--- bmi.js
+++ bmi.js
@@ -1,9 +1,9 @@
 
-function calculateBMI() {
+function CalculateBMI() {
   
  var mass = prompt("Enter your mass in kilograms:");
   var height =prompt("Enter your height in meters:");
  var bmi = mass / (height * height);
  console.log("Your BMI is: " + bmi.toFixed(2));
 }
-calculateBMI();
+CalculateBMI();
