dc869ecd65b611601a02777bea4518f4303fa763
blienmoges <blienmoges92@gmail.com>
2024-03-01 08:05:43 +0300

    edited Note Taking

---
+++ note/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/header'
+import Notes from './Components/notes'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <Header />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
